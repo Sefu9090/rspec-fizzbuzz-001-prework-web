86ee12888040be6b765f6e95fc68cf8e1061de96
Sefu9090 <fsaul16@yahoo.com>
2015-11-09 18:13:10 +0000

    nitrous-backup at Mon Nov  9 18:13:10 UTC 2015

---
+++ fizzbuzz.rb
@@ -0,0 +1,28 @@
+def fizzbuzz(num)
+  if (num % 3) === 0
+    "Fizz"
+  end
+end
+ 
+def fizzbuzz(num)
+  if (num % 5) === 0
+    "Buzz"
+  end
+end
+ 
+def fizzbuzz(num)
+  if (num % 3,5) === 0
+    "FizzBuzz"
+  end
+end
+ 
+def fizzbuzz(num)
+  if (num % 3) === 0
+    "Fizz"
+  end
+end
+ 
+fizzbuzz(3)
+fizzbuzz(5)
+fizzbuzz(15)
+fizzbuzz(4)
