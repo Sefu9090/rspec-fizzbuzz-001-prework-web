98cbaed961fdc4e9b1e760c27fae9dfc094a42f2
Sefu9090 <fsaul16@yahoo.com>
2015-11-12 20:48:24 +0000

    too done

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,28 +1,11 @@
 def fizzbuzz(num)
-  if (num % 3) === 0
-    "Fizz"
-  end
-end
- 
-def fizzbuzz(num)
-  if (num % 5) === 0
-    "Buzz"
-  end
-end
- 
-def fizzbuzz(num)
-  if (num % 3,5) === 0
+  if num % 3 === 0 && num % 5 === 0
     "FizzBuzz"
-  end
-end
- 
-def fizzbuzz(num)
-  if (num % 3) === 0
+  elsif num % 3 === 0
     "Fizz"
+  elsif num % 5 === 0
+    "Buzz"
+  else 
+    nil
   end
 end
- 
-fizzbuzz(3)
-fizzbuzz(5)
-fizzbuzz(15)
-fizzbuzz(4)
